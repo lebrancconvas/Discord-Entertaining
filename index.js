dc2ba2fdb027b56e03ca851577e54499197aa690
Poom Yimyuean <lebrancconvas@gmail.com>
2021-04-16 12:27:37 +0700

    :sparkles: Bot Command.

--- index.js
+++ index.js
@@ -18,8 +18,26 @@ bot.on('message', (message) => {
     let option = messagearray[2]
 
     if (cmd === `${prefix}hello`) {
-        message.reply("Hello, I'm ENTERTAIN MANNNN !!!!!")
+        message.reply("Hello, I'm Lady Ganbatte.\nThe Gaming Princess from the far far PopCulture Land.\nFeel free to live in this server ^^")
     }
+
+    if (cmd === `${prefix}join`) {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+                .then(connection => {
+                    message.reply('I have join to server')
+                })
+                .catch(console.log)
+        } else {
+            message.reply("You need to join to Voice Channel")
+        }
+    }
+
+    if (cmd === `${prefix}help`) {
+        message.reply("OK, I will help you ^^\nYou can type\n!hello: to greeting me.\n!join: to bring me to the voice channel you join.\n!help: to know about the command that you can give me to do.")
+    }
+
+    if (cmd === `${prefix}game`)
 })
 
 bot.login(conf.token)
